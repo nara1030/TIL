3ea6a78d7a5fe4f0d9007e611c207a42af1ea99f
Hong-Jun Eom <nara1030@naver.com>
2020-05-11 16:49:43 +0900

    JS로 CSS 적용

--- docs/lecture_list/inflearn/src/lotto.js
+++ docs/lecture_list/inflearn/src/lotto.js
@@ -27,16 +27,50 @@ console.log('당첨숫자(정렬): ' + lotto.win.sort(function(p, c) {
                                             }) + ", 보너스: " + lotto.bonus);
 
 var result = document.getElementById('result'); // 혹은 getElementsByTagName 사용
+var ball;
+function create_ball(num) { // 다른 부분 → 매개변수
+    ball = document.createElement('div');
+    ball.textContent = num;
+}
+function paint_ball(num) {
+    // 스타일(JS로 CSS 조작하기)
+    ball.style.display = 'inline-block';
+    ball.style.border = '1px solid black';
+    ball.style.borderRadius = '10px';   // abc-def-gh(CSS) → abcDefGh(JS)
+    ball.style.width = '20px';
+    ball.style.height = '20px';
+    ball.style.textAlign = 'center';
+    ball.style.marginRight = '10px';
+    ball.style.fontSize = '12px';
+    // 참고
+    // ball.id = '공ID' + num;      // HTML: id
+    // ball.className = "공CLASS" + num;    // HTML: class
+
+    // 빨간(1~10), 주황(11~20), 노란(21~30), 파란(31~40), 초록(41~45)
+    var ball_color;
+    if(num <= 10) {
+        ball_color = 'red';
+    } else if(num <= 20) {
+        ball_color = 'orange';
+    } else if(num <= 30) {
+        ball_color = 'yellow';
+    } else if(num <= 40) {
+        ball_color = 'blue';
+    } else {
+        ball_color = 'green';
+    }
+    ball.style.background = ball_color;
+}
 
 for(var i = 0; i < lotto.win.length; i++) {
     setTimeout(function() { // 1초간 간격두며 당첨번호 노출(클로저 이슈로 동작안함)
-        var ball = document.createElement('div');
-        ball.textContent = lotto.win[i];
+        create_ball(lotto.win[i]);
+        paint_ball(lotto.win[i]);
         result.appendChild(ball);
     }, 1000);
 }
 
 var bonus = document.getElementsByClassName('bonus')[0];
-ball = document.createElement('div');
-ball.textContent = lotto.bonus;
+create_ball(lotto.bonus);
+paint_ball(lotto.bonus);
 bonus.appendChild(ball);
