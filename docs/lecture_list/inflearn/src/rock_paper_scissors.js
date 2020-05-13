649f48455f8375cd5533d36064086e9353b3a906
Hong-Jun Eom <nara1030@naver.com>
2020-05-13 14:17:47 +0900

    승무패 제외 코드

---
+++ docs/lecture_list/inflearn/src/rock_paper_scissors.js
@@ -0,0 +1,48 @@
+var img = {
+    left: '0px',
+}
+
+// 컴퓨터
+var interval = setInterval(() => { // 화살표 함수
+    if(img.left === '0px') {
+        img.left = '-142px';
+    } else if(img.left === '-142px') {
+        img.left = '-284px';
+    } else {
+        img.left = '0px';
+    }
+    document.querySelector('#computer').style.background
+        = 'url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ' + img.left + ' 0';
+}, 100);
+
+// 사용자
+console.log(document.querySelectorAll('.btn')); // NodeList(3)
+console.log(document.querySelectorAll('.btn').length);  // 3
+document.querySelectorAll('.btn').forEach(function(button) {
+    button.addEventListener('click', function() {
+        // console.log(this.textContent, img.left);
+        console.log("man: " + this.textContent + ", " + "computer: " + inverse_dictionary(img.left)[0]);
+        clearInterval(interval);    // 이미지 고정
+        // 승무패
+
+        setTimeout(() => function() {   // 게임 재실행
+            interval();
+        }, 1000);
+    });
+});
+
+var dictionary = {  // 좌표, 승무패
+    rock: ['0px', 1],
+    scissor: ['-142px', 0],
+    paper: ['-284px', -1],
+}
+console.log(Object.entries(dictionary));
+function inverse_dictionary(img_left) { // 하드코딩 방지 함수
+    return Object.entries(dictionary).find(function(item_arr) {
+        // console.log(item_arr);
+        return item_arr[1][0] === img_left;
+    });
+}
+function judge_the_winner(man, computer) {
+
+}
