31f95b5e737c4fb9f39524f902c7a3f00840ef6c
Hong-Jun Eom <nara1030@naver.com>
2020-05-13 14:40:02 +0900

    리팩토링_게임재시작

--- docs/lecture_list/inflearn/src/rock_paper_scissors.js
+++ docs/lecture_list/inflearn/src/rock_paper_scissors.js
@@ -1,19 +1,10 @@
 var img = {
     left: '0px',
+    interval: '',
 }
 
 // 컴퓨터
-var interval = setInterval(() => { // 화살표 함수
-    if(img.left === '0px') {
-        img.left = '-142px';
-    } else if(img.left === '-142px') {
-        img.left = '-284px';
-    } else {
-        img.left = '0px';
-    }
-    document.querySelector('#computer').style.background
-        = 'url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ' + img.left + ' 0';
-}, 100);
+game_start();
 
 // 사용자
 console.log(document.querySelectorAll('.btn')); // NodeList(3)
@@ -22,11 +13,11 @@ document.querySelectorAll('.btn').forEach(function(button) {
     button.addEventListener('click', function() {
         // console.log(this.textContent, img.left);
         console.log("man: " + this.textContent + ", " + "computer: " + inverse_dictionary(img.left)[0]);
-        clearInterval(interval);    // 이미지 고정
+        clearInterval(img.interval);    // 이미지 고정
         // 승무패
 
-        setTimeout(() => function() {   // 게임 재실행
-            interval();
+        setTimeout(function() {   // 게임 재실행
+            game_start();
         }, 1000);
     });
 });
@@ -45,4 +36,17 @@ function inverse_dictionary(img_left) { // 하드코딩 방지 함수
 }
 function judge_the_winner(man, computer) {
 
+}
+function game_start() { // 게임 재실행을 위한 모듈화
+    img.interval = setInterval(() => { // 화살표 함수
+        if(img.left === '0px') {
+            img.left = '-142px';
+        } else if(img.left === '-142px') {
+            img.left = '-284px';
+        } else {
+            img.left = '0px';
+        }
+        document.querySelector('#computer').style.background
+            = 'url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ' + img.left + ' 0';
+    }, 100);
 }
